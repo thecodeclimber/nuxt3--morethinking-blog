c60f36f11ac722dd0695af55464b2a03d032fe22
thecodeclimber <65869221+thecodeclimber@users.noreply.github.com>
2022-08-01 02:08:02 +0300

    update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="h-screen">
+    <NuxtLayout />
+    <div class="bg-gray-100">
+      <NuxtPage />
+      <ContentDoc />
+    </div>
   </div>
 </template>
