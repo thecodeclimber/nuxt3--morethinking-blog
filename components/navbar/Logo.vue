6152fa3cc81892a314cb42cea94845bb6896ef8f
thecodeclimber <65869221+thecodeclimber@users.noreply.github.com>
2022-08-01 02:12:34 +0300

    update  components navbar Logo.vue

--- components/navbar/Logo.vue
+++ components/navbar/Logo.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="flex justify-start lg:w-0 lg:flex-1">
-    <a href="#">
-      <span class="sr-only">Workflow</span>
-      <img
-        class="h-8 w-auto sm:h-10"
-        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-        alt=""
-      />
-    </a>
+    <NuxtLink class="text-xl font-semibold text-gray-800" to="/"
+      >morethinking</NuxtLink
+    >
   </div>
 </template>
+
